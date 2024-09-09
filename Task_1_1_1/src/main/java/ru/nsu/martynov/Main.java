aa33f5cb119173c84693ba1999172ffc920c157a
Bogdan Martynov <b.martynov@g.nsu.ru>
2024-09-10 01:20:34 +0700

    Before delete reverse func

--- Task_1_1_1/src/main/java/ru/nsu/martynov/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/martynov/Main.java
@@ -3,9 +3,118 @@ package ru.nsu.martynov;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static int sum(int a, int b) {
-        return a + b;
+
+    public static int[] heapSort(int[] arr, boolean reverse) {
+        int n = arr.length;
+        printArray(arr);
+        int[] brr = new int[n];
+
+        // copy array
+        for (int i = 0; i < n; i++) {
+            brr[i] = arr[i];
+        }
+
+        // Do sorted heap.
+        // I can explain (n/2-1) in life (maybe draw binary tree).
+        for (int i = n-1; i >= 0; i--) {
+            siftDown(brr, i, n, reverse);
+        }
+        printArray(brr);
+
+        for (int i = n - 1; i >= 0; i--) {
+            swap(brr, 0, i, n);
+            siftDown(brr, 0, i, reverse); // do sift with brr[0], brr[i] is last elem in heap.
+        }
+
+        printArray(brr);
+        return brr;
     }
+
+    private static void siftUp(int[] arr, int i, int n, boolean reverse) {
+        int parent = (i-1)/2;
+
+        if (parent < 0 || i >= n)
+            return;
+
+        if (reverse) {
+            // min in UP (in last steps min in END)
+            if (arr[i] < arr[parent]) {
+                swap(arr, parent, i, n);
+            }
+        }
+        else {
+            // max in UP
+            if (arr[i] > arr[parent]) {
+                swap(arr, parent, i, n);
+            }
+        }
+    }
+
+    private static void siftDown(int[] arr, int i, int n, boolean reverse) {
+        if (i < 0 || i >= n) {
+            return;
+        }
+
+        int left = 2*i+1;
+        int right = 2*i+2;
+
+        if (reverse) {
+            int smallest = i;
+
+            // only if left in binary tree
+            if (left < n && arr[left] < arr[smallest]) {
+                smallest = left;
+            }
+            // only if right in binary tree
+            if (right < n && arr[right] < arr[smallest]) {
+                smallest = right;
+            }
+
+            // min in UP
+            if (arr[smallest] < arr[i]) {
+                swap(arr, i, smallest, n);
+            }
+        }
+        else {
+            int biggest = i;
+
+            // only if left in binary tree
+            if (left < n && arr[left] > arr[biggest]) {
+                biggest = left;
+            }
+            // only if right in binary tree
+            if (right < n && arr[right] > arr[biggest]) {
+                biggest = right;
+            }
+
+            // max in UP
+            if (arr[biggest] > arr[i]) {
+                swap(arr, i, biggest, n);
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        System.out.print("{");
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+        if (arr.length > 0) {
+            System.out.print(arr[arr.length - 1]);
+        }
+        System.out.print("}.\n");
+    }
+
+    private static void swap(int[] arr, int i, int j, int n) {
+        if (i < 0 || j < 0 || i >= n || j >= n) {
+            return;
+        }
+
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
     }
--- Task_1_1_1/src/test/java/ru/nsu/martynov/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/martynov/MainTest.java
@@ -4,11 +4,67 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Random;
+
 class MainTest {
+    // Generate random array
+    public static int[] randomArray(int size, int from, int to) {
+        Random rand = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = rand.nextInt(to - from) + from; // random number in [from, to)
+        }
+        return array;
+    }
+
+    private static boolean arrayIsSort(int[] arr, boolean reverse) {
+        boolean sorted = true;
+
+        if (reverse) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] < arr[i + 1]) {
+                    sorted = false;
+                    break;
+                }
+            }
+        }
+        else {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    sorted = false;
+                    break;
+                }
+            }
+        }
+
+        return sorted;
+    }
+
     @Test
-    void SampleTest() {
-        assertEquals(Main.sum(1, 2), 3);
-        assertEquals(Main.sum(2, 3), 5);
-        assertEquals(Main.sum(3, 3), 6);
+    void HeapSortTest() {
+        // Create 10 test arrays.
+        int[][] testArrays = new int[10][];
+
+        // Fill arrays different values.
+        testArrays[0] = new int[]{5, 3, 8, 6, 1, 9, 2, 7, 4}; // Unsorted array
+        testArrays[1] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}; // Sorted array
+        testArrays[2] = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1}; // Sorted-reversed array
+        testArrays[3] = new int[]{1}; // 1 elem
+        testArrays[4] = new int[0]; // Empty array
+        testArrays[5] = new int[]{5, 5, 5, 5, 5, 5, 5, 5, 5}; // All elems are same
+        testArrays[6] = new int[]{100, 20, -5, 200, 0, 50}; // Negatives and positives values.
+        testArrays[7] = randomArray(1, -1000, 1000); // Random array of length 10
+        testArrays[8] = randomArray(10, -1000, 1000); // Random array of length 1000
+        testArrays[9] = randomArray(10*10, -1000, 1000); // Random array of length 1kk
+
+        for (int i = 0; i < testArrays.length; i++) {
+            int[] tmp = Main.heapSort(testArrays[i], false);
+            Main.printArray(tmp);
+            assertTrue(arrayIsSort(tmp, false));
+
+            tmp = Main.heapSort(testArrays[i], true);
+            Main.printArray(tmp);
+            assertTrue(arrayIsSort(tmp, true));
+        }
     }
 }
