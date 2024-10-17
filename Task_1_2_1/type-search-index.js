6063c93aed904b112645c57bbd5b71fdb0dbb3e5
Hom4ikTop4ik <Hom4ikTop4ik@users.noreply.github.com>
2024-10-17 10:05:27 +0000

    Deploying to gh-pages from @ Hom4ikTop4ik/OOP@e4900f52f6c0035ded93bd1c4dede341629ab2b8 🚀

---
+++ Task_1_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"ru.nsu.martynov","l":"Adjacency"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.martynov","l":"Edge"},{"p":"ru.nsu.martynov","l":"General"},{"p":"ru.nsu.martynov","l":"Graph"},{"p":"ru.nsu.martynov","l":"Helper"},{"p":"ru.nsu.martynov","l":"Incidence"},{"p":"ru.nsu.martynov","l":"ListGraph"}]
