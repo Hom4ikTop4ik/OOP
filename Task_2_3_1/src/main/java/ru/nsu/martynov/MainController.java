7ddc4fc7b0e5bb3058bd4620fec3e20ee0af3322
Bogdan Martynov <b.martynov@g.nsu.ru>
2025-05-08 15:22:37 +0700

    microTest

--- Task_2_3_1/src/main/java/ru/nsu/martynov/MainController.java
+++ Task_2_3_1/src/main/java/ru/nsu/martynov/MainController.java
@@ -3,14 +3,21 @@ package ru.nsu.martynov;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class MainController {
 
     @FXML
-    private Button btn;
+    private Button btn1;
     private int x = 0;
     @FXML
     private void click(ActionEvent event) {
-        btn.setText(String.format("%d", x++));
+        btn1.setText(String.format("%d", ++x));
+    }
+    @FXML
+    private void spend(ActionEvent event) {
+        if (x >= 10) {
+            btn1.setText(String.format("%d", x-=10));
+        }
     }
 }
